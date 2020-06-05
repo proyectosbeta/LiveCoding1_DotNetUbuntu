17146046e49602c717121884e885e12d77aa2ca6
cmasdev <christianamado@outlook.com>
2020-06-05 15:05:25 -0400

    First Commit

---
+++ consola/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace consola
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¡Hola mundo!");
+
+            Program.ImprimirFecha();
+
+            ////Para que se quede abierta la consola (En Windows)
+            //Console.ReadKey();
+        }
+
+        private static void ImprimirFecha()
+        {
+            var fecha = new DateTime(2020,05,23);
+
+            Console.WriteLine(fecha.ToString("yy-dd-MMMM"));
+        }
+    }
+}
